94eaf7ebd017c46d19205ebf850bbc398a7c18f7
Khanh Tran <forbaokhanh@gmail.com>
2023-10-20 12:29:13 -0400

    [feat]: cleaning up new data parsing logic

--- src/script.py
+++ src/script.py
@@ -1,93 +1,91 @@
-from typing import Tuple, Optional
+from typing import Tuple, Dict
+from rich import pretty, print
 import pandas as pd
 import re
-import math
-
-# Define the path to the CSV file
-file_path = "./data/stuff.csv"
 
 
-def parse_entry(entry: str) -> Tuple[int, str]:
+class Ingredient:
+    def __init__(self, name):
+        self.name = name
+        self.rating_info = {}
+
+    def set_rating(self, rating, concentration: float = 1.0):
+        self.rating_info[concentration] = rating
+
+    def get_rating(self, concentration: float = 1.0):
+        return self.rating_info[concentration]
+
+    def __repr__(self):
+        return f"""Ingredient(Name: {self.name}, Ratings: {self.rating_info})"""
+
+
+def parse_concentration(input_str: str) -> Tuple[str, float]:
+    # Use regex to find the postfix that matches the pattern (10%)
+    match = re.search(r'\((\d+)%\)$', input_str)
+    if match:
+        # Extract the percentage value and convert it to a float
+        percentage = float(match.group(1)) / 100
+
+        # Extract the prefix by removing the postfix from the input string
+        prefix = input_str[:match.start()].strip()
+
+        return prefix, percentage
+    return input_str.strip(), 1.0
+
+
+def parse_rating(input_str: str) -> int:
     """
-    Parse an entry string to extract the name and the maximum rating value.
-
-    Parameters:
-    - entry (str): The entry string to parse. Should be in the format 'Name (Rating)'.
-      Special characters like '*' and '^' are ignored. Rating can be a single integer or a range (e.g., 0-2).
-
-    Returns:
-    - Tuple[int, str]: A tuple containing two elements:
-        1. The maximum rating as an integer. If the rating is a range (e.g., 0-2), the maximum value is returned.
-        2. The name as a sanitized string, with leading and trailing whitespaces removed.
-
-    Raises:
-    - Exception: If the entry string doesn't match the expected format, an exception is raised with a descriptive message.
-
-    Example:
-    >>> parse_entry("John Doe (2-5)")
-    (5, "John Doe")
-
-    >>> parse_entry("Jane^ Doe* (3)")
-    (3, "Jane Doe")
-
-    >>> parse_entry("Invalid Entry")
-    Exception: Couldn't parse entry Invalid Entry
+    Hacky solution.
+    :param input_str: a string formatted like (1) or (0-5)
+    :return: the rating approximation integer, in case of presented range, take the maximum
     """
-    # Skip if entry is None or nan
-    if entry is None or (isinstance(entry, float) and math.isnan(entry)):
-        return None, None
+    return int(input_str[-2])
 
-    if not isinstance(entry, str):
-        raise Exception(f"Expected a string or bytes-like object. Got {type(entry)}")
 
-    # Remove characters like '*', '^' from the entry
-    sanitized_entry = re.sub(r"[\*\^]", "", entry)
-
-    # Extract name and rating using regular expression
-    match = re.match(r"^(.+)\s?\(([0-9\-]+)\)$", sanitized_entry)
+def parse_entry(entry: str) -> Ingredient:
+    # Use regex to find the postfix that matches the pattern (1) or (0-5).
+    # The digits are always within the range 0-5 (inclusive), and the format allows either:
+    # - A single digit (e.g., (1))
+    # - Two digits separated by a single dash (e.g., (3-4))
+    match = re.search(r'\((\d-?\d?)\)$', entry)
 
     if match:
-        name, rating = match.groups()
+        postfix = match.group(0)  # The whole match including the parenthesis
+        rating = parse_rating(postfix)
 
-        # Check if rating has a range (e.g., 0-2)
-        if "-" in rating:
-            min_rating, max_rating = map(int, rating.split("-"))
-            max_rating = max(min_rating, max_rating)  # Choose the maximum value
-        else:
-            max_rating = int(rating)
+        # Remove the postfix from the original entry to get the first part
+        first_part = entry.replace(postfix, '').strip()
 
-        return max_rating, name.strip()
+        # Handle cases for elements that present with different concentrations
+        first_part, concentration = parse_concentration(first_part)
+
+        ingredient = Ingredient(first_part)
+        ingredient.set_rating(rating, concentration)
+        return ingredient
 
     else:
-        raise Exception("Couldn't parse entry {}".format(entry))
+        raise Exception(f"Couldn't find a valid postfix, missing comedogenic rating info for {entry}")
+
+
+def process_raw_csv_file(filepath: str) -> Dict[str, Ingredient]:
+    df = pd.read_csv(filepath, sep=",")
+
+    ingredients = {}
+
+    # Iterating over each cell
+    for index, row in df.iterrows():
+        for col in df.columns:
+            cell_value = row[col]
+            if isinstance(cell_value, str):
+                ingredient = parse_entry(cell_value)
+                ingredients[ingredient.name] = ingredient
+    return ingredients
 
 
 if __name__ == "__main__":
-    # Read the CSV file into a Pandas DataFrame
-    try:
-        df = pd.read_csv(file_path, sep=",")
-        db = {}
+    pretty.install()
 
-        # Iterating over each cell
-        for index, row in df.iterrows():
-            for col in df.columns:
-                cell_value = row[col]
-                rating, name = parse_entry(cell_value)
-                if rating is None:
-                    continue
-                if rating not in db:
-                    db[rating] = []
-                db[rating].append(name)
-
-        print(db)
-
-    except FileNotFoundError:
-        print(f"File not found at {file_path}")
-        exit(1)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        exit(1)
-
-    # Display the first few rows of the DataFrame
-    print(df.head())
-    print(df.info())
+    # Define the path to the CSV file
+    file_path = "./data/stuff.csv"
+    cache = process_raw_csv_file(file_path)
+    print(cache)
